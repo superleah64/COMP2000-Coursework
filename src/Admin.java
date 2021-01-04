ce961424d828f85d40dcda4efaf65beac0c1d9d5
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2021-01-04 13:47:12 +0000

    Created admin form
    Created form for the admin page and pseudocode for an if statement to notify staff to replenish stock

---
+++ src/Admin.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Admin {
+    private JTextArea stockDatabase;
+    private JPanel panel1;
+
+    // if (stock = 0
+    //  System.out.println("[itemName] is out of stock. Please replenish the stock.");
+    // );
+
+}
